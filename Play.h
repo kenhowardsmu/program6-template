8f041b70d9ec263609399bbd292e75354da77d6c
kenhowardsmu <kenhoward@smu.edu>
2023-04-11 15:52:26 -0500

    initial version

---
+++ Play.h
@@ -0,0 +1,18 @@
+//
+// Created by Ken Howard on 4/11/23.
+//
+
+#ifndef PROGRAM6_SPRING2023_PLAY_H
+#define PROGRAM6_SPRING2023_PLAY_H
+#include <string>
+#include <iostream>
+#include "Play.h"
+#include <ctime>
+using namespace std;
+
+class Play {
+
+
+};
+
+#endif //PROGRAM6_SPRING2023_PLAY_H
---
+++ Team.h
@@ -0,0 +1,18 @@
+//
+// Created by Ken Howard on 4/11/23.
+//
+
+#ifndef PROGRAM6_SPRING2023_TEAM_H
+#define PROGRAM6_SPRING2023_TEAM_H
+#include <string>
+#include <vector>
+#include "Play.h"
+using namespace std;
+
+class Team {
+
+
+
+};
+
+#endif //PROGRAM6_SPRING2023_TEAM_H
